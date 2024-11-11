8e61c4607fc6b11857e3f9dac6643381c685fe9e
Your Name <mmogire11@gmail.com>
2024-11-11 20:12:32 +0300

    news card component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import NewsCard from "./components/NewsCard";
 
 function App() {
 
   return (
     <>
-      <div className="container mx-auto  dark:text-neutral-100 ">
-        <p className="text-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo dignissimos dicta ratione omnis, ea odit quae natus veritatis quasi possimus esse aliquid magnam porro alias rem rerum impedit quis aliquam?</p>
+      <div className="~px-4/16 mx-auto  ">
+        <NewsCard/>
       </div>
     </>
   )
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,7 +1,12 @@
 export default function NewsCard() {
     return (
-        <div className="NewsCard-wrapper">
-            
-        </div>
-    )
+      <div className="NewsCard-wrapper">
+        <p className="text-bold">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
+          dignissimos dicta ratione omnis, ea odit quae natus veritatis quasi
+          possimus esse aliquid magnam porro alias rem rerum impedit quis
+          aliquam?
+        </p>
+      </div>
+    );
 }
